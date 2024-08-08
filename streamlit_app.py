73fe226137c4c1e30be720af6074633a171ef120
lam-01 <105969643+lam-01@users.noreply.github.com>
2024-08-08 23:55:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('Dự đoán kết quả học tập của học sinh ')
 
 st.write('Đây là app dự đoán kết quả')
+df=pd.read_csv('https://raw.githubusercontent.com/lam-01/Data/main/Student_performance_data_2.csv' )
