4c9312880cb34da94c250443888f862038adc5f6
lam-01 <105969643+lam-01@users.noreply.github.com>
2024-08-09 09:24:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,34 @@ y
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='Absences', y='GPA',color='GradeClass')
   st.scatter_chart(data=df, x='StudyTimeWeekly', y='GPA',color='GradeClass')
+
+
+with st.sidebar:
+  st.header('Input features')
+   Gender = st.selectbox('Gender', ( 'Male', 'Famale'))
+   Ethnicity = st.selectbox('Ethnicity',( 'Caucasian', 'African American', 'Asian', 'Other'))
+   ParentalEducation = st.selectbox('ParentalEducation', ('None', 'High School', 'Some College', 'Bachelor, 'Higher')
+   Tutoring = st.selectbox('Tutoring',( 'Yes','No'))
+   ParentalSupport = st.selectbox('ParentalSupport',( 'None', 'Low', 'Moderate', 'High', 'Very High'))
+   Extracurricular = st.selectbox('Extracurricular',( 'Yes','No'))
+   Volunteering = st.selectbox('Ethnicity',( 'Yes','No'))
+   Tutoring = st.selectbox('Ethnicity',( 'Yes','No'))
+  
+#   # Create a DataFrame for the input features
+#   data = {'island': island,
+#           'bill_length_mm': bill_length_mm,
+#           'bill_depth_mm': bill_depth_mm,
+#           'flipper_length_mm': flipper_length_mm,
+#           'body_mass_g': body_mass_g,
+#           'sex': gender}
+#   input_df = pd.DataFrame(data, index=[0])
+#   input_penguins = pd.concat([input_df, X_raw], axis=0)
+
+# with st.expander('Input features'):
+#   st.write('**Input penguin**')
+#   input_df
+#   st.write('**Combined penguins data**')
+#   input_penguins
   
 
 
