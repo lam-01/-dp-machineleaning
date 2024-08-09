25ad732dd5cd503a2b2f3eea20422a0a69c618f1
lam-01 <105969643+lam-01@users.noreply.github.com>
2024-08-09 08:03:42 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,16 @@
 import streamlit as st
+import numpy as np
 import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
 
-st.title('Dự đoán kết quả học tập của học sinh ')
+st.title('🤖 Machine Learning App')
 
-st.info('Đây là app dự đoán kết quả')
+st.info('This is app builds a machine learning model!')
+
 with st.expander('Data'):
-   st.write('**Raw data**')
-df=pd.read_csv('https://raw.githubusercontent.com/lam-01/Data/main/Student_performance_data_2.csv' )
-df
+  st.write('**Raw data**')
+  df = pd.read_csv('https://raw.githubusercontent.com/lam-01/Data/main/Student_performance_data_2.csv')
+  df
 
 
 # st.write('**X**')
