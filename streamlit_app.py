6c705711ff4c2b8eb012f526f6a9857d777b8866
lam-01 <105969643+lam-01@users.noreply.github.com>
2024-08-09 08:01:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,16 @@ st.title('Dự đoán kết quả học tập của học sinh ')
 
 st.info('Đây là app dự đoán kết quả')
 with st.expander('Data'):
-     st.write('**Raw data**')
+   st.write('**Raw data**')
 df=pd.read_csv('https://raw.githubusercontent.com/lam-01/Data/main/Student_performance_data_2.csv' )
 df
 
 
-st.write('**X**')
-X=df.drop('StudentID',axis=1)
-X
+# st.write('**X**')
+# X=df.drop('StudentID',axis=1)
+# X
 
-st.write('**y**')
-y=df.StudentID
-y
+# st.write('**y**')
+# y=df.StudentID
+# y
 
