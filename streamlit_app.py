a72cad790fd000498ddfea2da052173b3daa1954
lam-01 <105969643+lam-01@users.noreply.github.com>
2024-08-13 21:50:07 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,11 +62,11 @@ X_res, y_res = smote.fit_resample(X_train, y_train)
 st.write(f"Train set size after SMOTE: {X_res.shape[0]} samples")
 
 # Encode y
-target_mapper = {'0': A ,
-                 '1': B ,
-                 '2': C ,
-                 '3': D ,
-                 '4': F ,
+target_mapper = {'0': 'A' ,
+                 '1': 'B' ,
+                 '2': 'C' ,
+                 '3': 'D' ,
+                 '4': 'F' ,
                 }
 def target_encode(val):
   return target_mapper[val]
