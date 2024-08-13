25f56a6866586a875ea2c936a74e1cc48a7737e2
lam-01 <105969643+lam-01@users.noreply.github.com>
2024-08-13 21:21:31 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,6 +60,64 @@ smote = SMOTE(sampling_strategy='auto')
 X_res, y_res = smote.fit_resample(X_train, y_train)
 
 st.write(f"Train set size after SMOTE: {X_res.shape[0]} samples")
+with st.sidebar:
+    st.header('Input features')
+
+    gender_map = {"Male": 0, "Female": 1}
+    gender_selected = st.selectbox('Gender', ('Male', 'Female'))
+    gender_encoded = gender_map[gender_selected]
+
+    ethnicity_map = {"Caucasian": 0, "African American": 1, "Asian": 2, "Other": 3}
+    ethnicity_selected = st.selectbox('Ethnicity', ('Caucasian', 'African American', 'Asian', 'Other'))
+    ethnicity_encoded = ethnicity_map[ethnicity_selected]
+
+    parental_education_map = {"None": 0, "High School": 1, "Some College": 2, "Bachelor": 3, "Higher": 4}
+    parental_education_selected = st.selectbox('ParentalEducation', ('None', 'High School', 'Some College', 'Bachelor', 'Higher'))
+    parental_education_encoded = parental_education_map[parental_education_selected]
+
+    tutoring_map = {"Yes": 1, "No": 0}
+    tutoring_selected = st.selectbox('Tutoring', ('Yes', 'No'))
+    tutoring_encoded = tutoring_map[tutoring_selected]
+
+    parental_support_map = {"None": 0, "Low": 1, "Moderate": 2, "High": 3, "Very High": 4}
+    parental_support_selected = st.selectbox('ParentalSupport', ('None', 'Low', 'Moderate', 'High', 'Very High'))
+    parental_support_encoded = parental_support_map[parental_support_selected]
+
+    extracurricular_map = {"Yes": 1, "No": 0}
+    extracurricular_selected = st.selectbox('Extracurricular', ('Yes', 'No'))
+    extracurricular_encoded = extracurricular_map[extracurricular_selected]
+
+    volunteering_map = {"Yes": 1, "No": 0}
+    volunteering_selected = st.selectbox('Volunteering', ('Yes', 'No'))
+    volunteering_encoded = volunteering_map[volunteering_selected]
+
+    study_time_weekly = st.number_input('Study Time Weekly (hours)', min_value=0, max_value=20)
+    absences = st.number_input('Absences', min_value=0, max_value=30)
+
+    # Create a DataFrame for the input features
+    data = {
+        'Gender': gender_encoded,
+        'Ethnicity': ethnicity_encoded,
+        'ParentalEducation': parental_education_encoded,
+        'Tutoring': tutoring_encoded,
+        'ParentalSupport': parental_support_encoded,
+        'Extracurricular': extracurricular_encoded,
+        'Volunteering': volunteering_encoded,
+        'StudyTimeWeekly': study_time_weekly,
+        'Absences': absences,
+       
+    }
+
+
+input_df = pd.DataFrame(data, index=[0])
+input_penguins = pd.concat([input_df, X], axis=0)
+
+with st.expander('Input features'):
+  st.write('**Input data**')
+  st.dataframe(input_df)
+  st.write('**Combined data**')
+  st.dataframe(input_penguins)
+
 
 ######################################### LOAD MÔ HÌNH ###########################################
 
