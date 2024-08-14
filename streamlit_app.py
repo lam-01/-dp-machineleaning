bb57c3718e27abfb3a86ee739ac2cfc6af215e68
lam-01 <105969643+lam-01@users.noreply.github.com>
2024-08-14 19:35:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,7 @@ st.title("Phân Tích và Dự Báo Điểm GPA")
 
 # with st.expander('Data'):
 #     st.write('**Raw data**')
+with : 
     df = pd.read_csv('https://raw.githubusercontent.com/lam-01/Data/main/Student_performance_data_2.csv')
     st.write(df)
 
