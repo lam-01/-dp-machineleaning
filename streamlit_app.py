1ad0260d9b344a809031d6e65908229189a1b1fb
lam-01 <105969643+lam-01@users.noreply.github.com>
2024-08-14 21:10:58 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ X_train, X_test, y_train, y_test = train_test_split(X_res, y_res, test_size=0.2,
 
  
 # Xây dựng
-with st.sidebar():
+with st.sidebar:
     st.header('Input features')
 
     gender_map = {"Male": 0, "Female": 1}
